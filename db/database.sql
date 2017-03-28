367f8146e7ce8f2dd60a896d1746359840cad349
alucard371 <test8q@gmail.com>
2017-03-28 12:03:02 +0200

    changed database name

--- db/database.sql
+++ db/database.sql
@@ -1,4 +1,4 @@
-create database if not exists mybooks character set utf8 collate utf8_unicode_ci;
-use mybooks;
+create database if not exists mybook character set utf8 collate utf8_unicode_ci;
+use mybooks-test;
 
 grant all privileges on mybooks.* to 'mybooks_user'@'localhost' identified by 'secret';
